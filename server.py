9def87895d2eda0158fef814553e5a922170947e
Tomas Rydenstam <tomas@rydenstam.se>
2024-02-19 12:49:21 +0100

    Initial commit

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
+import csv
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'somthing went wrong, try again'
+
+
+def write_to_file(data):
+    with open('database.txt', 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(
+            f'\ndate: {datetime.now()}\nfrom: {email}\nsubject: {subject}\nmessage: {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
